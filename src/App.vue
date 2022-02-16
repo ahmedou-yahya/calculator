f21deacba2bbe1690b8a308bb9cdbaced7b5758f
19092 <19092@esp.mr>
2022-02-16 13:37:04 +0000

    simple calculator

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <Input @input-change="updateInput1"/>
+    </div>
+    <div class="row">
+      <select  name="operator" id="" v-model="operator">
+      <option  value="+" :selected="operator">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+    </div>
+    <div class="row">
+      <Input @input-change="updateInput2"/>
+    </div>
+    <div class="row">=</div>
+    <div>{{result}}</div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import Input from "@/components/Input.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Input,
+  },
+  data() {
+    return {
+      firstOperator: 0,
+      operator: "+",
+      secondOperator: 0,
+    }
+  },
+  methods: {
+    updateInput1(val){
+      this.firstOperator = val;
+    },
+    updateInput2(val){
+      this.secondOperator = val;
+    }
+  },
+  computed: {
+    result(){
+      let result = 0;
+      switch(this.operator){
+        case '+':
+          result = this.firstOperator + this.secondOperator;
+          break;
+        case '-':
+          result = this.firstOperator - this.secondOperator;
+          break;
+        case '*':
+          result = this.firstOperator * this.secondOperator;
+          break;
+        case '/':
+          if(this.secondOperator===0){
+            return "You can't divide by zero"
+          }
+          result = this.firstOperator / this.secondOperator;
+      }
+      return result;
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+.container{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 100px;
+  padding: 100px;
+  background-color: #BBE6D6;
+  border-radius: 5px;
 }
 </style>
+
---
+++ src/components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="number" v-model="input" @change="onInputChange">
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data() {
+    return {
+      input: 0,
+    }
+  },
+  methods:{
+    onInputChange(){
+      this.$emit('input-change', this.input)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
